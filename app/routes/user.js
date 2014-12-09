9baafab4324e470ef6eef34bf8150204abff991c
vikram-s-narayan <vikram@freado.com>
2014-12-08 19:16:44 -0800

    Added User route and template

---
+++ app/routes/user.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+var UserRoute =
+ Ember.Route.extend({
+   model: function(params) {
+     return this.store.find('user', params.user_id);
+   }
+});
+
+export default UserRoute;
