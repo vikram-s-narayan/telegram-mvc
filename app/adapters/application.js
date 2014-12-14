7564a66fed239fbe4d38a454d251f717c694dc73
vikram-s-narayan <vikram@freado.com>
2014-12-13 22:34:28 -0800

    Changed to REST adapter.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
-var ApplicationAdapter = DS.FixtureAdapter.extend();
+//var ApplicationAdapter = DS.FixtureAdapter.extend();
+var ApplicationAdapter = DS.RESTAdapter.extend({
+  namespace: 'api',
+  host: 'http://192.168.56.10:3000'
+});
 
 export default ApplicationAdapter;
