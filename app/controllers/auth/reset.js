31570f7e8d36c8eae0fe5d685880a4da17930b34
vikram-s-narayan <vikram@freado.com>
2015-01-29 08:00:59 -0800

    Reset password with email sending done. But route redirection not yet done.

--- app/controllers/auth/reset.js
+++ app/controllers/auth/reset.js
@@ -8,7 +8,7 @@ export default Ember.Controller.extend({
       var _this = this;
       //because we're asking to find 'user', it goes to users route on server
       this.store.find('user', {operation: 'passwordReset', email: email}).then(function(user){
-        _this.transitionToRoute('auth.checkinbox');
+        _this.transitionToRoute('mystream');
         alert(user);
       });
       alert(email);
